0e0e02bede1f07d4cd2ebbad1c723cfb3cc77d13
snehal <snehal@DESKTOP-NJ5I0QH>
2022-06-11 17:11:54 +0530

    Added additional checks....

--- DemoGitProject/src/demopackage/Test2.java
+++ DemoGitProject/src/demopackage/Test2.java
@@ -1,11 +1,11 @@
-package demopackage;
+
 
 public class Test2 {
 
 	public static void main(String[] args) 
 	{
 		String hungry = "yes";
-		if(hungry == "yes")
+		if(hungry == "yes" || hungry == "Y" || hungry=="y") 
 			System.out.println("eat something");
 		else
 		{
